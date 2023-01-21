1c6296d95000fe295d8a2c547165d0b6bd8d650d
Marcelo Carvalho <mpirescarvalho17@gmail.com>
2023-01-20 21:13:31 -0300

    initial commit

---
+++ web/tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        background: '#09090A',
+      },
+
+      gridTemplateRows: {
+        7: 'repeat(7, minmax(0, 1fr))',
+      },
+
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
